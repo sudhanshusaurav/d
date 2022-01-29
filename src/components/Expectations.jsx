fac83e70214ed92340157aef535b824be05a3c91
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-01-29 15:32:16 +0530

    swiper color change

--- src/components/Expectations.jsx
+++ src/components/Expectations.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Exp from '../assets/PSD_013.png'
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination"
 
-// import "./styles.css";
+import './expectation.css'
 
-
-// import Swiper core and required modules
 import SwiperCore, {Pagination} from 'swiper';
 SwiperCore.use([Pagination]);
 
 function Expectations() {
+    useEffect(() => {
+      const swipperBullets = document.querySelectorAll('.swiper .swiper-pagination-bullet')
+      swipperBullets.forEach(bullet => {
+            bullet.addEventListener('click', function(){
+                bullet.classList.add('bullet-active')
+            })          
+      });
+    }, []);
+    
   return <div className='w-[90%] mx-auto p-8'>
       <h2 className='text-5xl my-8'>Exceeding <br /> Expectations</h2>
       <Swiper spaceBetween={30} pagination={{
