1de9e987312adab1711dbede17f07adb066a4a35
sudhanshu_saurav <sudhanshussaurav@gmail.com>
2022-01-29 14:25:15 +0530

    menu cursor fix

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,11 +5,11 @@ function Menu({menuActive}) {
   return <nav className={`fixed top-0 left-0 w-full h-full bg-black/10 menu ${menuActive ? 'menu--open' : ''}`}>
       <ul className='flex flex-col items-start justify-start p-16 h-full w-3/4 lg:w-2/3 bg-white ml-auto'>
           <li className='mb-8 text-xl text-black/50'>menu</li>
-        <li className=''><Link to='' className='nav__link slider-text' data-text="Process">Process</Link></li>
-        <li className=''><Link to='' className='nav__link slider-text' data-text="Work">Work</Link></li>
-        <li className=''><Link to='' className='nav__link slider-text' data-text="Service">Service</Link></li>
-        <li className=''><Link to='' className='nav__link slider-text' data-text="Us">Us</Link></li>
-        <li className=''><Link to='' className='nav__link slider-text' data-text="Contact">Contact</Link></li>
+        <li className=''><Link to='' className='nav__link hoverable' data-text="Process">Process</Link></li>
+        <li className=''><Link to='' className='nav__link hoverable' data-text="Work">Work</Link></li>
+        <li className=''><Link to='' className='nav__link hoverable' data-text="Service">Service</Link></li>
+        <li className=''><Link to='' className='nav__link hoverable' data-text="Us">Us</Link></li>
+        <li className=''><Link to='' className='nav__link hoverable' data-text="Contact">Contact</Link></li>
         <li className='mt-8'><Link to='' className='nav__link text-xl text-black/50 hoverable'>Get in Touch</Link></li>
       </ul>
   </nav>;
